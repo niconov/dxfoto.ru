49345a69f9bcdfc3fb0cf79406bcdb7abdd6b3cc
Dmitry Grigoriev <dmitry@dxfoto.ru>
2016-04-11 22:34:32 +0300

    Still no luck, trying to force-feed env vars to aws-sdk

--- _plugins/load_meta.rb
+++ _plugins/load_meta.rb
@@ -2,7 +2,8 @@ require 'aws-sdk'
 require 'fileutils'
 Jekyll::Hooks.register :site, :after_reset do |site|
   unless Dir.entries(site.config['source'] + '/_data/').include? 'tags'
-    Aws.config.update region: 'eu-west-1'
+    Aws.config.update region: 'eu-west-1',
+                      credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
     s3 = Aws::S3::Client.new
     resp = s3.list_objects(bucket: 'meta-dxfoto-ru')
     resp.contents.each do |m|
