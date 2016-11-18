5e2666d640a1014462c831a923c8d058af79479e
Dmitry Grigoriev <dmitry@dxfoto.ru>
2016-11-18 22:09:28 +0300

    Better coordinates transformation

---
+++ _plugins/coord_format.rb
@@ -0,0 +1,10 @@
+module Jekyll
+  # filter to format EXIF GPS coordinates for Google Maps
+  module CoordFormatFilter
+    def coord_format(input)
+      input.tr(' deg', '°').tr('"', '″').tr("'", '′')
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::CoordFormatFilter)
